d71a1aa986f30b95b44d2fdb0a8678454312fffd
Neil Young <neiljosephyoung@gmail.com>
2023-04-25 21:26:11 +0100

    first commit

--- App.js
+++ App.js
@@ -1,36 +1,150 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity  } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView, Button  } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { RNCamera } from 'react-native-camera';
 
-const App = () => {
+export default function App() {
+  const [url, setUrl] = useState('https://dog.ceo/api/breeds/image/random');
+  const [method, setMethod] = useState('GET');
+  const [response, setResponse] = useState('');
 
-  const [name, setName] = useState('');
-  const [gender, setGender] = useState('');
+  const [showBarcodeScanner, setShowBarcodeScanner] = useState(false);
+
+  const handleRequest = async () => {
+    try {
+      const options = {
+        method: method
+      }
+      const res = await fetch(url, options);
+      const json = await res.json();
+      setResponse(JSON.stringify(json));
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>User Information</Text>
-      <TextInput
-        placeholder="Enter name"
-        value={name}
-        onChangeText={text => setName(text)}
-        style={{ borderWidth: 1, borderColor: '#ccc', padding: 10, width: '80%', marginBottom: 20 }}
-      />
-      <Picker
-        selectedValue={gender}
-        onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
-        style={{ borderWidth: 1, borderColor: '#ccc', padding: 10, width: '80%', marginBottom: 20 }}
-      >
-        <Picker.Item label="Select Gender" value="" />
-        <Picker.Item label="Male" value="male" />
-        <Picker.Item label="Female" value="female" />
-        <Picker.Item label="Other" value="other" />
-      </Picker>
-      <TouchableOpacity style={{ backgroundColor: '#007bff', padding: 10, borderRadius: 5 }}>
-        <Text style={{ color: '#fff', fontSize: 18 }}>Submit</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.navbar}>
+        <Text style={styles.navbarText}>API Tester</Text>
+      </View>
+      <ScrollView>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>URL:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setUrl(text)}
+            value={url}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Method:</Text>
+          <Picker
+            selectedValue={method}
+            onValueChange={(value) => setMethod(value)}
+            style={styles.picker}
+          >
+            <Picker.Item label="GET" value="GET" />
+            <Picker.Item label="POST" value="POST" />
+            <Picker.Item label="PUT" value="PUT" />
+
+          </Picker>
+        </View>
+       
+        <TextInput
+          style={styles.response}
+          onChangeText={text => setResponse(text)}
+          value={response}
+          placeholder="Response"
+          multiline={true}
+          numberOfLines={10}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleRequest}
+        >
+          <Text style={styles.buttonText}>Send Request</Text>
+        </TouchableOpacity>
+
+        <View style={{ paddingBottom: 16 }}>
+        <Button
+          title="Barcode Scanner"
+          onPress={() => setShowBarcodeScanner(!showBarcodeScanner)}
+        />
+
+        
+      </View>
+      {showBarcodeScanner && (
+        <View style={{ flex: 1 }}>
+          <RNCamera
+            style={{ flex: 1 }}
+            onBarCodeRead={(barcode) => console.log(barcode.data)}
+          />
+        </View>
+      )}
+
+      </ScrollView>
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingTop: 50,
+  },
+  navbar: {
+    backgroundColor: '#333',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navbarText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    marginVertical: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  label: {
+    marginRight: 10,
+    marginTop: 40
+  },
+  input: {
+    marginTop: 40,
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 5,
+  },
+  picker: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: '#007aff',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 5,
+    marginTop: 10,
+    
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  },
+  response: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 5,
+    marginTop: 10,
+    height: 200,
+  },
+});
